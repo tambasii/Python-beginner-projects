5f55c6b6a4ff553cb97684d19148f78676e42b54
tambasii <wenslastambasi@gmail.com>
2024-11-01 00:16:21 +0300

    Wrote the first game. Took answers from user, checked if they were correct and awarded a score and a percentage at the end.

---
+++ quizgame.py
@@ -0,0 +1,52 @@
+print ("Welcome to my Simple Quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play")
+score = 0
+
+answer = input ("What does CPU stands for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input ("What does GPU stands for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+
+else:
+    print("Incorrect!")
+
+answer = input ("What does RAM stands for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+
+else:
+    print("Incorrect!")
+
+answer = input ("What does UPS stands for? ")
+if answer.lower() == "uninterruptible power supply":
+    print("Correct!")
+    score += 1
+
+else:
+    print("Incorrect!")
+
+answer = input ("What language is this? ")
+if answer.lower() == "python":
+    print("Correct!")
+    score += 1
+
+
+else:
+    print("Incorrect!")
+
+print ("You got " + str(score) + " questions correct")
+print ("You got " + str((score/4 *100)) + "%.")
